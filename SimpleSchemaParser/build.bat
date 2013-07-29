5515f9da2ba442c6d078ce958a72c1facc54f98e
Jaap Taal <jaap@q42.nl>
2013-07-29 13:38:52 +0200

    version nr and nuget package

---
+++ SimpleSchemaParser/build.bat
@@ -0,0 +1,2 @@
+"C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" /p:Configuration=Release /t:Rebuild SimpleSchemaParser.csproj
+nuget pack SimpleSchemaParser.csproj -Prop Configuration=Release
